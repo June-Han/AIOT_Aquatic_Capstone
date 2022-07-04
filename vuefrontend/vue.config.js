01472df3ad3e5ea7ad322506d05179b25a2f5385
June-Han <julialiu08@gmail.com>
2022-07-04 20:35:37 +0800

    changes for pages deploy

--- vuefrontend/vue.config.js
+++ vuefrontend/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/AIOT_Aquatic_Capstone/' //for github pages
 })
